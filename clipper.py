3ca1d86549602b45bb28581211b38e6d3cee7136
Krzysztof Włódarczak <krzysztofprzem@gmail.com>
2020-03-06 19:35:04 +0100

    Corrected algorithm. Now is working fine :)

--- clipper.py
+++ clipper.py
@@ -5,10 +5,11 @@ import numpy as np
 
 
 
-def cut_audio_clip(input_path, start, end, output_path, Fs):
+def cut_audio_clip(input_path, start, end, output_path):
     audio = wave.read(input_path)
-    audio = np.array(audio[1], dtype=float)/(65536/2)
-    clip_audio = audio[start:end][:,0]
+    samples = np.array(audio[1], dtype=float)/(65536/2)
+    Fs = audio[0]
+    clip_audio = samples[start:start + end*Fs][:,0]
     # n = []
     # print(clip_audio)
     # for i in range(len(audio)):
@@ -21,8 +22,7 @@ def cut_audio_clip(input_path, start, end, output_path, Fs):
 if __name__ == "__main__":
     input_path = "./audio3_orig.wav"
     output_path = "audio3_clip.wav"
-    Fs = 44100
     number_of_samples = 5000
     start = 0
-    end = start + 60*Fs
-    cut_audio_clip(input_path, start, end, output_path, Fs)
+    end = 60
+    cut_audio_clip(input_path, start, end, output_path)
--- fastica.py
+++ fastica.py
@@ -1,151 +1,200 @@
 import scipy.io.wavfile as wave
+import scipy.optimize
 from matplotlib import pyplot as plt
 import numpy as np
-import scipy.optimize
 
-
-# org = wave.read("org.wav")
-# short = wave.read("short2.wav")
-# long = wave.read("long2.wav")
-
-
-# print(org)
-
-# print(len(org))
-
-# org = np.array(org[1], dtype=float)/(65536/2)
-# short = np.array(short[1], dtype=float)/(65536/2)
-# long = np.array(long[1], dtype=float)/(65536/2)
-
-def draw_distribution(x, points=-1, plotname="test.jpg", printing=True, showing=True):
+def draw_distribution(x, points=-1, plotname="test.jpg", printing=True, show=True, save=True):
     fig = plt.figure()
+    plt.plot(x[0][:points], x[1][:points], "bo")
     plt.plot([-100000, 100000],[0,0], "k")
     plt.plot([0,0],[-100000, 100000], "k")
-    plt.plot(x[0][:points], x[1][:points], "bo")
-    plt.plot(0,0, "ro")
+    plt.plot(0,0, "ko")
     plt.grid()
     plt.xlim([min(x[0][:points])*1.2, max(x[0][:points])*1.2])
     plt.ylim([min(x[1][:points])*1.2, max(x[1][:points])*1.2])
     plt.xlabel("audio1")
     plt.ylabel("audio2")
-    if showing:
+    if show:
         plt.show()
-    fig.savefig(plotname, dpi=fig.dpi)
+    if save:
+        fig.savefig(plotname, dpi=fig.dpi)
         
 
-
-
-
-def load_samples(input_path):
+def load_audios(input_path):
     audio = wave.read(input_path)
-    audio = np.array(audio[1], dtype=float)
-    return audio
+    samples = np.array(audio[1], dtype=float)
+    Fs = audio[0]
+    return samples, Fs
 
 
-
-def load_audios(input_paths):
-    s = []
+def load_samples(input_paths):
+    x = []
+    Fs = 0
     for input_path in input_paths:
-        audio = load_samples(input_path)
-        print("MAX:", max(audio))
-        s.append(audio)
-    s = np.array(s)
-    return s
+        audio, Fs = load_audios(input_path)
+        # print("MAX:", max(audio))
+        x.append(audio)
+    x = np.array(x)
+    return x, Fs
 
-def save_audios(output_paths, z):
+
+def save_audios(output_paths, z, Fs):
     for i in range(len(output_paths)):
-        print("MAX:", max(z[i]))
-        wave.write(output_paths[i], 44100, z[i])
+        # print("MAX:", max(z[i]))
+        wave.write(output_paths[i], Fs, z[i])
 
 
-def mixing(input_paths, output_paths, show=True):
-    audios_number = len(input_paths)
-    s = load_audios(input_paths)
-    if show:
-        draw_distribution(s)
+def mixing(x, printing=True, show=True, save=True):
+    audios_number = len(x)
+    if show or save:
+        draw_distribution(x, plotname="distribution_sources.jpg", printing=printing, show=show, save=save)
+    
+    # A = np.random.random_integers(1,5,(audios_number, audios_number))
     A = np.random.random_sample((audios_number, audios_number))
-    y = np.dot(A, s)
-    save_audios(output_paths, y)
-    if show:
-        draw_distribution(y)
+    for i in range(len(A)):
+        A[i] *= 1/np.linalg.norm(A[i])
+        
+    if printing:
+        print("A:")
+        print(A)
+        print("inversed A:")
+        print(np.linalg.inv(A))
+        print()
+
+    y = np.dot(A, x)
+    if show or save:
+        draw_distribution(y, plotname="distribution_mixed.jpg", printing=printing, show=show, save=save)
+    return y
 
 
-def centering(y):
+
+
+def centering(y, printing=True):
+    if printing:
+        print("mean before centering: ", np.mean(y[0]), np.mean(y[1]))
+
     new_y = y.copy()
     means = []
+
     for i in range(len(y)):
         mean = np.mean(y[i])
         means.append(mean)
         new_y[i] -= mean
     means = np.array(means)
+
+    if printing:
+        print("mean after centering:  ", np.mean(new_y[0]), np.mean(new_y[1]))
+        print()
+
     return new_y, means
 
 
-def whitening(y):
+def whitening(y, printing=True, show=True, save=True):
     y_t = np.transpose(y)
+    #covariance=np.cov(np.dot(y, y_t))
     covariance=np.cov(y)
     D, V = np.linalg.eigh(covariance)
     D = np.diag(D)
     V_t = np.transpose(V)
+
+    if printing:
+        print("covariance matrix: ")
+        print(covariance)
+        print("covariance matrix V*D*V_t: ")
+        print(np.dot(V, np.dot(D, V_t)))
+        
     D[D!=0]=D[D!=0]**(-1/2)
     A_w = np.dot(V, np.dot(D, V_t))
     z = np.dot(A_w, y)
+
+    if printing:
+        print("covariance matrix after whitening:\n", np.cov(z))
+        for i in range(len(z)):
+            print("covariance of signal "+str(i)+" after whitening: ", np.cov(z[i]))
+        print()
+
+    if show or save:
+        draw_distribution(z, plotname="distribution_whitened.jpg", printing=printing, show=show, save=save)
+
     return z
 
 
+def w_init(z, printing=True):
+    w = np.random.random_sample((z.shape[0]))
+    w /= np.linalg.norm(w)
+    w = np.transpose(w)
+    if printing:
+        print("norm of initialized w:", np.linalg.norm(w))
+        print()
+    return w
+
+
 def return_v(v, w_t):
-    output = np.dot(w_t, v)
+    output = abs(np.dot(w_t, v))*10000
     output += abs(np.linalg.norm(v)-1)*1000
     return output
 
+def determine_v(w_t, printing=True):
+    v_init = np.array([-5, 10])
+    v = scipy.optimize.fmin(return_v, v_init, args=(w_t,), disp=False)
+    v_t = np.transpose(v)
+    if printing:
+        print("multiplication of w_t and v_t:  ", np.dot(w_t, v))
+        print("norm of v:\t\t\t", np.linalg.norm(v))
+        print("matrix with w_t and v_t:")
+        print(np.array([w_t, v_t]))
+        print()
+    return v
 
-def fastICA(input_paths, output_paths, iterations=25, eps=0.000001):
-    x = load_audios(input_paths)
-    # CENTERING
-    x, _ = centering(x)
-    # WHITENING
-    z = whitening(x)
 
-    w_old = np.random.random_sample((x.shape[0]))
-    w_magnitude = np.linalg.norm(w_old)
-    w_old *= 1/w_magnitude
-    w_old = np.transpose(w_old)
-    w_old_t = np.transpose(w_old)
-
-    # ITERIATIONS :>
+def fastICA(y, iterations=25, eps=0.000001, printing=True, show=True, save=True):
+    print("centering")
+    y, _  = centering(y, printing=printing)
+    print("whitening")
+    z     = whitening(y, printing=printing, show=show, save=save)
+    print("determining w vector")
+    w_old = w_init(z, printing=printing)
+    # ITERATIONS :>
     for i in range(iterations):
-        print("iteration:", i)
-        w = np.average(np.dot(z,((np.dot(w_old_t, z)))**3)) - 3*w_old
-        w_magnitude = np.linalg.norm(w)
-        w *= 1/w_magnitude
+        if printing:
+            print("iteration:", i)
+        
+        temp = z *(np.dot(np.transpose(w_old), z)**3)
+        averages = []
+        for j in range(len(temp)):
+            averages.append(np.average(temp[j]))
+        averages = np.array(averages)
+        w = averages - 3*w_old
+
+        w *= 1/np.linalg.norm(w)
         w_t = np.transpose(w)
-        if abs(1-np.dot(w_t, w_old))<eps:
-            print(np.dot(w_t, w_old))
+        if abs(1-np.dot(w_t, w_old)) < eps:
+            if printing:
+                print("multiplication of w_t, w_old:   ", np.dot(w_t, w_old))
+                print()
             break
         w_old = w
 
-    v = scipy.optimize.fmin(return_v, np.ones(w_old.shape), args=(w_t,))
+    v = determine_v(w_t, printing=printing)
     v_t = np.transpose(v)
 
-    x_e = np.dot(np.array([w_t, v_t]), z)
+    A_inv = np.array([w_t, v_t])
+    x_e = np.dot(A_inv, z)
 
-
-    save_audios(output_paths, x_e)
+    return x_e
 
 
 
 if __name__ == "__main__":
-    input_paths = ["audio1_clip.wav", "audio2_clip.wav", "audio3_clip.wav"]
-    mixed_paths = ["mixed1.wav", "mixed2.wav", "mixed3.wav"]
-    output_paths = ["audio1_estimated.wav", "audio3_estimated.wav", "audio3_estimated.wav"]
+    input_paths = ["audio2_clip.wav", "audio3_clip.wav"]
+    mixed_paths = ["mixed2.wav", "mixed3.wav"]
+    output_paths = ["audio2_estimated.wav", "audio3_estimated.wav"]
 
-    input_paths = ["audio1_clip.wav", "audio2_clip.wav"]
-    mixed_paths = ["mixed1.wav", "mixed2.wav"]
-    output_paths = ["audio1_estimated.wav", "audio2_estimated.wav"]
+    x, Fs = load_samples(input_paths)
+    y = mixing(x, printing=True, show=False, save=True)
+    save_audios(mixed_paths, y, Fs)
 
-    # A = np.array([1,2,3,4,5,6])
-    # B = np.array([1,2,3,4,5,6])
-    # print(np.dot(A,B))
-
-    #mixing(input_paths, mixed_paths, False)
-    fastICA(input_paths, output_paths)
+    
+    y, Fs = load_samples(mixed_paths)
+    x_e = fastICA(y, printing=True, show=False, save=True)
+    save_audios(output_paths, x_e, Fs)
